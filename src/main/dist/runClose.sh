beee24541b624f86ab25b42f267ab5c8be23f827
llamersmcw <llamers@mcw.edu>
2024-10-15 11:48:52 -0500

    Update runClose.sh

--- src/main/dist/runClose.sh
+++ src/main/dist/runClose.sh
@@ -8,6 +8,6 @@ cd $APPDIR
 
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
-    -jar lib/$APPNAME.jar -close"$@" > run.log 2>&1
+    -jar lib/$APPNAME.jar -close "$@" > run.log 2>&1
 
 mailx -s "[$SERVER] genes-from-snps Run" llamers@mcw.edu < $APPDIR/logs/summary.log
